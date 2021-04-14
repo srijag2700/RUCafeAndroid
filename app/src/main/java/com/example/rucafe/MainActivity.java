496dc05b210fabceb3acd80c6b64ac794cd566a3
Srija Gottiparthi <srija.gottiparthi@gmail.com>
2021-04-13 20:48:24 -0400

    figured out basic intent stuff

--- app/src/main/java/com/example/rucafe/MainActivity.java
+++ app/src/main/java/com/example/rucafe/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.rucafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button donutsButton = findViewById(R.id.orderDonuts);
+        donutsButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DonutActivity.class);
+            startActivity(intent);
+        });
+
+        Button coffeeButton = findViewById(R.id.orderCoffee);
+        coffeeButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CoffeeActivity.class);
+            startActivity(intent);
+        });
+
+        Button viewButton = findViewById(R.id.viewOrder);
+        viewButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ViewOrderActivity.class);
+            startActivity(intent);
+        });
+
+        Button allButton = findViewById(R.id.allOrders);
+        allButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AllOrdersActivity.class);
+            startActivity(intent);
+        });
     }
 }
